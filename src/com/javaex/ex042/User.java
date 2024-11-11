7f802fb8fa681b548bc7531b901dfd95ea3505f7
미스터도스 <38653351+MrDos89@users.noreply.github.com>
2024-11-11 09:35:51 +0900

    예제 추가

---
+++ src/com/javaex/ex042/User.java
@@ -0,0 +1,11 @@
+package com.javaex.ex042;
+
+public class User {
+	private String id;
+	private String password;
+	private String name;
+	
+	public User() {
+		
+	}
+}
--- src/com/javaex/ex07/FriendInfo.java
+++ src/com/javaex/ex07/FriendInfo.java
@@ -15,6 +15,31 @@ public class FriendInfo {
 		this._school = school;
 	}
 	
+	//getter, setter 작성
+	public String getName() {
+		return _name;
+	}
+	
+	public void setName(String name) {
+		this._name = name;
+	}
+	
+	public String getPhone() {
+		return _phone;
+	}
+	
+	public void setPhone(String phone) {
+		this._phone = phone;
+	}
+	
+	public String getSchool() {
+		return _school;
+	}
+	
+	public void setSchool(String school) {
+		this._school = school;
+	}
+	
 	public void showInfo() {
 		System.out.printf("이름: %s \t 핸드폰:%s \t 학교:%s%n", _name, _phone, _school);
 	}
